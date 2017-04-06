c4df4acbd73c7afd550a5da7085deb7d766fb7e2
zqczx00 <zhanqichao@8think.cn>
2017-04-06 15:27:47 +0800

    al

---
+++ 0329/vue_example.js
@@ -0,0 +1,32 @@
+var getChildrenTextContent = function (children) {
+	return children.map(function (node) {
+		return node.children ? getChildrenTextContent(node.children) : node.text;
+	}).join('');
+}
+
+Vue.component('anchored-heading', {
+	render: function (createElement) {
+		// create kebabCase id
+		var headingId = getChildrenTextContent(this.$slots.default)
+						.toLowerCase()
+						.replace(/\W+/g, '-')
+						.replace(/^\-|\-$/g, '');
+		return createElement(
+			'h' + this.level,
+			[
+				createElement('a', {
+					attrs: {
+						name: headingId,
+						href: '#' + headingId
+					}
+				}, this.$slots.default)
+			]
+		)
+	},
+	props: {
+		level: {
+			type: Number,
+			required: true
+		}
+	}
+})
